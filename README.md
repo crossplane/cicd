36309b14186881eb7f22071f158b289a23b6a7b8
suskin <813571+suskin@users.noreply.github.com>
2020-02-19 15:52:34 -0800

    jenkins: add shared Jenkins pipelines
    We want to be able to re-use common Jenkins logic across multiple
    projects. One way to do that is by reusing entire pipelines. In our
    case, we are able to do this because our pipelines are identical across
    multiple repositories.
    
    Signed-off-by: suskin <813571+suskin@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,27 @@
+# Stack CICD
+
+This repository has shared functionality for stack ci/cd jobs.
+
+## Usage
+
+Create a `Jenkinsfile` which loads the Jenkins Pipeline libraries
+from this repo, then call into the pipeline you want to run.
+
+Here is how to dynamically load and call the branch pipeline from a
+Jenkinsfile:
+
+```groovy
+// Load a library dynamically. For more detail, see:
+// https://jenkins.io/doc/book/pipeline/shared-libraries/#defining-declarative-pipelines
+library identifier: 'stack-cicd@master', retriever: modernSCM(
+  [$class: 'GitSCMSource',
+   remote: 'https://github.com/suskin/stack-cicd',
+   credentialsId: 'github-upbound-jenkins'])
+
+// This is the name of a file declared in vars/{{fileName}}.groovy in the library repository.
+runStackBranchPipeline()
+```
+
+There are other ways to load shared libraries. See [the Jenkins Pipeline
+documentation](https://jenkins.io/doc/book/pipeline/shared-libraries/)
+for more detail.
---
+++ vars/runStackContinuousPipeline.groovy
@@ -0,0 +1,52 @@
+def call() {
+    // The continuous pipeline is used to build and publish new versions of the stack when changes are merged into
+    // the master branch.
+    pipeline {
+        agent { label 'upbound-gce' }
+
+        options {
+            disableConcurrentBuilds()
+            timestamps()
+        }
+
+        // Checks for unprocessed changes in the repo once per day
+        // 'H' allows Jenkins to choose the time to balance the load
+        triggers {
+            pollSCM('H H * * *')
+        }
+
+        environment {
+            DOCKER = credentials('dockerhub-upboundci')
+            CROSSPLANE_CLI_RELEASE = 'master'
+        }
+
+        stages {
+            stage('Prepare') {
+                steps {
+                    sh 'mkdir bin'
+                    sh "curl -sL https://raw.githubusercontent.com/crossplane/crossplane-cli/${CROSSPLANE_CLI_RELEASE}/bootstrap.sh | env PREFIX=${WORKSPACE} RELEASE=${CROSSPLANE_CLI_RELEASE} bash"
+                }
+            }
+            stage('Promote Release') {
+
+                steps {
+                    // The build step turns this into a "dirty" environment from the perspective of `git describe`,
+                    // so we set the version once at the beginning and use it for both the build and publish steps.
+
+                    sh """STACK_VERSION=$( git describe --tags --dirty --always )
+                          STACK_VERSION=\${STACK_VERSION} ./bin/kubectl-crossplane-stack-build
+                          STACK_VERSION=\${STACK_VERSION} ./bin/kubectl-crossplane-stack-publish
+                    """
+                }
+            }
+        }
+
+        post {
+            always {
+                script {
+                    deleteDir()
+                }
+            }
+        }
+    }
+}
