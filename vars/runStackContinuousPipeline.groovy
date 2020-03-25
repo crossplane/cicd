43522fa7aa7fe9d86adaa8f82ea9f2677dcff574
suskin <813571+suskin@users.noreply.github.com>
2020-03-25 16:07:34 -0700

    versioning: auto-increment continuous build versions
    When we build an artifact from our continuous build job which runs
    against changes to `master`, we want to be able to consistently have a
    prerelease semantic version, which follows the semantic version ordering
    contract. To do this, we can generate and manipulate the version which
    is used for the artifact. We use the most recent tag in the current
    branch as a base.
    
    As part of this change, we also now expect the publish pipeline to
    **not** generate a version; an operator must specify the parameter by
    hand.
    
    Signed-off-by: suskin <813571+suskin@users.noreply.github.com>

--- vars/runStackContinuousPipeline.groovy
+++ vars/runStackContinuousPipeline.groovy
@@ -28,6 +28,7 @@ def call() {
                 steps {
                     sh 'mkdir bin'
                     sh "curl -sL https://raw.githubusercontent.com/crossplane/crossplane-cli/${CROSSPLANE_CLI_RELEASE}/bootstrap.sh | env PREFIX=${WORKSPACE} RELEASE=${CROSSPLANE_CLI_RELEASE} bash"
+                    sh "curl -sL -o bin/semver https://raw.githubusercontent.com/fsaintjacques/semver-tool/3.0.0/src/semver && chmod +x bin/semver"
                 }
             }
             stage('Publish Release') {
@@ -36,7 +37,23 @@ def call() {
                     // The build step turns this into a "dirty" environment from the perspective of `git describe`,
                     // so we set the version once at the beginning and use it for both the build and publish steps.
 
-                    sh """STACK_VERSION=\$( git describe --tags --dirty --always )
+                    // The lines which calculate the stack version are a bit complicated, so here are some examples
+                    // of the final version output for different scenarios.
+                    //
+                    // * No tags, one commit in the repository: v0.1.0-rc-1-gdeadbee
+                    // * No tags, ten commits in the repository: v0.1.0-rc-10-gdeadbee
+                    // * Most recent tag v0.1.0, target commit is same as tag: v0.2.0-rc-0-gdeadbee
+                    // * Most recent tag v0.1.0, target commit is 2 commits after the tag: v0.2.0-rc-2-gdeadbee
+                    // * Most recent tag v0.1.0-rc, target commit is 2 commits after the tag: v0.2.0-rc-2-gdeadbee
+                    sh """STACK_VERSION=\$( git describe --tags --long || echo "v0.0.0-rc-\$( git log --oneline | wc -l | xargs echo )-g\$( git rev-parse --short=7 HEAD )" )
+                          STACK_PREREL=\$( bin/semver get prerel \${STACK_VERSION} )
+
+                          # In the case that our most recent tag was something like v0.1.0, our `git describe` output
+                          # will look like 'v0.1.0-2-gdeadbee'. This makes our prerelease segment look more like
+                          # 'rc-2-gdeadbee', instead of '2-gdeadbee'.
+                          STACK_PREREL=\$( echo \${STACK_PREREL} | sed -e 's/\\(^[0-9]\\)/rc-\\1/' )
+
+                          STACK_VERSION="v\$( bin/semver bump minor \${STACK_VERSION} )-\${STACK_PREREL}"
                           STACK_VERSION=\${STACK_VERSION} ./bin/kubectl-crossplane-stack-build
                           STACK_VERSION=\${STACK_VERSION} ./bin/kubectl-crossplane-stack-publish
                     """
--- vars/runStackPublishPipeline.groovy
+++ vars/runStackPublishPipeline.groovy
@@ -4,7 +4,7 @@ def call() {
         agent { label 'upbound-gce' }
 
         parameters {
-            string(name: 'version', defaultValue: '', description: 'The version you are publishing. For example: v0.4.0. If left unspecified, the build will generate one for you.')
+            string(name: 'version', description: 'Required. The version you are publishing. For example: v0.4.0.')
         }
 
         options {
@@ -21,20 +21,25 @@ def call() {
         }
 
         stages {
+            stage('Check Required Parameters') {
+                steps {
+                    sh "test -n '${params.version}' || ( echo 'ERROR: The version is required. Please specify a version to release.' >&2 && exit 1 )"
+                    sh "( echo '${params.version}' | grep -q -E '^v' - ) || ( echo 'ERROR: The version needs to start with a \"v\". Example: \"v0.1.0\".' >&2 && exit 1 )"
+                }
+            }
+
             stage('Prepare') {
                 steps {
                     sh 'mkdir bin'
                     sh "curl -sL https://raw.githubusercontent.com/crossplane/crossplane-cli/${CROSSPLANE_CLI_RELEASE}/bootstrap.sh | env PREFIX=${WORKSPACE} RELEASE=${CROSSPLANE_CLI_RELEASE} bash"
                 }
             }
+
             stage('Publish Release') {
 
                 steps {
-                    // The build step turns this into a "dirty" environment from the perspective of `git describe`,
-                    // so we set the version once at the beginning and use it for both the build and publish steps.
-
-                    sh """STACK_VERSION=${params.version}
-                          STACK_VERSION=\${STACK_VERSION:-\$( git describe --tags --dirty --always )}
+                    sh """
+                          STACK_VERSION=${params.version}
                           STACK_VERSION=\${STACK_VERSION} ./bin/kubectl-crossplane-stack-build
                           STACK_VERSION=\${STACK_VERSION} ./bin/kubectl-crossplane-stack-publish
                     """
